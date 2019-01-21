36d9353353a4600968c83d9af8ea2667e3ea9ebf
coderdba <coderdba@coder.com>
2019-01-21 12:38:13 +0530

    updates

---
+++ playbooks/roles1/run.sh
@@ -0,0 +1,5 @@
+#ansible-playbook -vvv -i inv/group1.inv pb1.yml
+ansible-playbook -i inv/group1.inv pb1.yml
+
+#ansible-playbook -i inv/group2.inv all1.yml
+#ansible-playbook -i inv/group2.inv --tags osprint all1.yml 
